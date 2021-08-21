c4b04a8598ac316d40ddd0b8eb0b0c0b06fafba5
Taitat <taita.omori@gmail.com>
2021-08-21 19:05:07 +0900

    Modified to handle Multi-letter punctuators.

--- 9cc.c
+++ 9cc.c
@@ -21,6 +21,7 @@ struct Token {
   Token *next; // nextはToken型のポインタ。次のトークンのメモリ位置が入っている。
   int val;
   char *str;
+  int len;
 };
 
 // Token型のポインタをグローバル変数として定義
@@ -50,10 +51,12 @@ void error_at(char *loc, char *fmt, ...) { // 可変長引数
   exit(1);
 }
 
-bool consume(char op){
+bool consume(char *op){
   // 今着目しているトークンのkindが記号ではないか、
   // トークン文字列の1文字目が引数で期待した記号でない場合は偽を返す
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+  if (token->kind != TK_RESERVED || 
+      strlen(op) != token->len ||   // 渡ってきた記号と今のトークンのlenメンバの一致。先にこちらを比較することで'>='のような複数文字かどうかを検証する
+      memcmp(token->str, op, token->len))  // 渡ってきた記号と今のトークンのstrメンバのメモリブロックを、今のトークンのバイト数分比較
     return false;
   // 期待した記号なら着目するトークンを次の要素に進める
   token = token->next;
@@ -62,8 +65,10 @@ bool consume(char op){
 
 // トークンが期待している記号の場合はトークンを次に進める
 // そうでないならエラーを報告する
-void expect(char op){
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+void expect(char *op){
+  if (token->kind != TK_RESERVED || 
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len)) 
     error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
@@ -85,12 +90,17 @@ bool at_eof() {
 
 // 新しいトークンを作成して、curに繋げる
 /* curやstrはなぜポインタ？ */
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
   Token *tok = calloc(1, sizeof(Token));   // 新しいトークンのためのメモリ領域をゼロクリア
   tok->kind = kind;   
   tok->str = str;
+  tok->len = len;
   cur->next = tok; // 今作ったトークンを、着目しているトークンのnextへ繋げる
   return tok;
+}
+
+bool startwith(char *p, char *q) {
+  return memcmp(p, q, strlen(q)) == 0;
 }
 
 // 入力文字列のトークナイズ
@@ -108,9 +118,18 @@ Token *tokenize() { // *p には渡された文字列の1文字目が入る
       continue;
     }
 
+    // 複数文字から成る演算子
+    if (startwith(p, "==") || startwith(p, "!=") || 
+        startwith(p, "<=") || startwith(p, ">=")) {
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    }
+
+    // 1文字から成る演算子
     // 記号がきたときは記号のトークンを作成して、curに繋げる
-    if (strchr("+-*/()", *p)) {
-      cur = new_token(TK_RESERVED, cur, p++); /* なぜpをインクリメントする？ */ 
+    if (strchr("+-*/()<>", *p)) {
+      cur = new_token(TK_RESERVED, cur, p++, 1); /* なぜpをインクリメントする？ */ 
       continue;
     }
 
@@ -118,15 +137,19 @@ Token *tokenize() { // *p には渡された文字列の1文字目が入る
     // 数字と認識できるところまでを現在のトークンのvalへ格納し、
     // 残りをpのアドレスを使ってp自体に格納する
     if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p);
-      cur->val = strtol(p, &p, 10); 
+      cur = new_token(TK_NUM, cur, p, 0);
+      // pを書き換える前に一旦qへコピーしておく
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      // 数字をとって残ったpのアドレスとqの差分をトークンの長さとしてlenメンバへセット
+      cur->len = p - q; 
       continue;
     }
 
     error_at(p, "invalid token");
   }
   
-  new_token(TK_EOF, cur, p);
+  new_token(TK_EOF, cur, p, 0);
   return head.next;
 }
 
