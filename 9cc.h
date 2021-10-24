669c219ef493952d3344e91715227b1ede7dca7f
Taitat <taita.omori@gmail.com>
2021-10-25 07:46:03 +0900

    ローカル変数のためのNodeKind及び、Node構造体に変数領域を示すoffset属性を追加

--- 9cc.h
+++ 9cc.h
@@ -54,24 +54,27 @@ Token *tokenize();
 //
 
 typedef enum {
-  ND_ADD, // +
-  ND_SUB, // -
-  ND_MUL, // *
-  ND_DIV, // /
-  ND_EQ,  // ==
-  ND_NE,  // !=
-  ND_LT,  // <
-  ND_LE,  // <=
-  ND_NUM, // Integer
+  ND_ADD,    // +
+  ND_SUB,    // -
+  ND_MUL,    // *
+  ND_DIV,    // /
+  ND_EQ,     // ==
+  ND_NE,     // !=
+  ND_LT,     // <
+  ND_LE,     // <=
+  ND_ASSIGN, // =
+  ND_LVAR,   // ローカル変数
+  ND_NUM,    // Integer
 } NodeKind;
 
 // AST node type
 typedef struct Node Node;
 struct Node {
-  NodeKind kind;
-  Node *lhs;
-  Node *rhs;
-  int val;
+  NodeKind kind;  // ノードの型
+  Node *lhs;      // 左辺
+  Node *rhs;      // 右辺
+  int val;        // kindがND_NUMの場合のみ使う
+  int offset;     // kindがND_LVARの場合のみ使う
 };
 
 Node *new_node(NodeKind kind);
