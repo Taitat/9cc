44b9ecaacdd446d547042503d31e00269af507a6
Taitat <taita.omori@gmail.com>
2021-10-25 07:50:41 +0900

    main関数のプロローグで変数領域を確保する処理を追加

--- main.c
+++ main.c
@@ -8,19 +8,34 @@ int main(int argc, char **argv) {
   // Tokenize and parse.
   user_input = argv[1];
   token = tokenize();
-  Node *node = expr();
+  program();
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".globl _main\n");
   printf("_main:\n");
 
-  // 抽象構文木を降りながらコード生成
-  gen(node);
 
+  // プロローグ
+  // 変数26個分の領域を確保する
+  printf("  push rbp\n");
+  printf("  mov rbp, rsp\n");
+  printf("  sub rsp, 208\n");
+
+  // 先頭の式から順にコード生成
+  for (int i = 0; code[i]; i++) {
+    gen(code[i]);
+    // 式の評価結果としてスタックに一つの値が残っている
+    // はずなので、スタックが溢れないようにポップしておく
+    printf("  pop rax\n");
+  }
+  
+  // エピローグ
+  // 最後の式の結果がRAXに残っているのでそれが返り値になる
   // スタックトップに式全体の値が残っているはずなので、
   // それをRAXにロードして関数からの返り値とする。
-  printf("  pop rax\n");
+  printf("  mov rsp, rbp\n");
+  printf("  pop rbp\n");
   printf("  ret\n");
   return 0;
 }
